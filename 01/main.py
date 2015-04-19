c2410996e11739d26b2ec0adcf667a7868a876d4
pumpkin233 <wxs7410@yeah.net>
2015-04-19 21:05:02 +0800

    commit

---
+++ 01/main.py
@@ -0,0 +1,7 @@
+﻿for i in range(9,0,-1):
+  for k in range(0,i-1):    
+      print(end="")
+  for j in range(9,0,-1):
+      m=str.format("{0:1}*{1:1}={2:<2}",i,j,i*j) 
+      print(m,end=" ")
+  print()
